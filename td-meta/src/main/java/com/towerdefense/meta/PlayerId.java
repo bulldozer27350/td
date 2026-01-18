13a883ca3a410d91b1b82a3b2fde3df81b4175e5
Marc Olivier ETOURNEAU <mo.etourneau@gmail.com>
2026-01-18 01:08:02 +0100

    Un peu de ménage de code inutile

--- td-meta/src/main/java/com/towerdefense/meta/PlayerId.java
+++
@@ -1,9 +0,0 @@
-package com.towerdefense.meta;
-
-import java.util.UUID;
-
-public record PlayerId(UUID value) {
-    public static PlayerId random() {
-        return new PlayerId(UUID.randomUUID());
-    }
-}
