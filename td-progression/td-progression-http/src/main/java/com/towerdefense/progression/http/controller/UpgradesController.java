958c1d0e139f13bfe3b6b652bfdd090eee54a875
Marc Olivier ETOURNEAU <mo.etourneau@gmail.com>
2026-01-27 06:01:07 +0100

    Le rendu HTML des progression n'affichait aucune progression car les effets étaient null. C'est corrigé ici

--- td-progression/td-progression-http/src/main/java/com/towerdefense/progression/http/controller/UpgradesController.java
+++ td-progression/td-progression-http/src/main/java/com/towerdefense/progression/http/controller/UpgradesController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.towerdefense.progression.domain.upgrade.TowerUpgrade;
+import com.towerdefense.progression.http.model.UpgradeEffect;
 import com.towerdefense.progression.http.api.UpgradesApi;
 import com.towerdefense.progression.http.model.PurchaseUpgradeResponse;
 import com.towerdefense.progression.http.model.UpgradeInfo;
@@ -41,10 +42,23 @@ public class UpgradesController implements UpgradesApi {
 		upgradeInfo.setTowerLevel(towerUpgrade.getTowerLevel());
 		upgradeInfo.setTowerTypeId(towerUpgrade.getTowerTypeId());
 		upgradeInfo.setUnlocked(unlocked);
+		upgradeInfo.setEffect(toUpgradeEffect(towerUpgrade.getEffect()));
 		return upgradeInfo;
 	}
 
-	@Override
+	private UpgradeEffect toUpgradeEffect(com.towerdefense.progression.domain.upgrade.UpgradeEffect effect) {
+	    UpgradeEffect upEffect = new UpgradeEffect();
+	    upEffect.setStat(effect.stat());
+	    upEffect.setModifier(effect.modifier());
+	    switch (effect.type()) {
+            case ADD -> upEffect.setType(UpgradeEffect.TypeEnum.ADD);
+            case MULTIPLY -> upEffect.setType(UpgradeEffect.TypeEnum.MULTIPLY);
+            default -> upEffect.setType(null);
+            }
+	    return upEffect;
+    }
+
+    @Override
 	public ResponseEntity<List<UpgradeInfo>> getUpgradesForTower(@PathVariable("towerTypeId") String towerTypeId) {
 		var unlockedIds = metaGameService.getPlayerProgress().getUnlockedUpgrades();
 		
