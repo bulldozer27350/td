b6dcec000db488432a84419d2571ca278bb7915c
Marc Olivier ETOURNEAU <mo.etourneau@gmail.com>
2026-04-18 07:56:45 +0200

    Fix dependency injection in PlayerController

--- td-progression/td-progression-http/src/main/java/com/towerdefense/progression/http/controller/PlayerController.java
+++ td-progression/td-progression-http/src/main/java/com/towerdefense/progression/http/controller/PlayerController.java
@@ -10,28 +10,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.towerdefense.progression.domain.game.GameConfig;
 import com.towerdefense.progression.http.api.PlayerApi;
+import com.towerdefense.progression.http.config.ReloadableBeansManager;
 import com.towerdefense.progression.http.mapper.PlayerHttpMapper;
 import com.towerdefense.progression.http.model.AvailableLevel;
 import com.towerdefense.progression.http.model.GameConfiguration;
 import com.towerdefense.progression.http.model.LevelCompletionRequest;
 import com.towerdefense.progression.http.model.LevelCompletionResponse;
 import com.towerdefense.progression.http.model.PlayerProgressResponse;
-import com.towerdefense.progression.service.MetaGameService;
 
 @RestController
 public class PlayerController implements PlayerApi {
 
-    private final MetaGameService metaGameService;
+    private final ReloadableBeansManager beansManager;
     private final PlayerHttpMapper mapper;
 
-    public PlayerController(MetaGameService metaGameService, PlayerHttpMapper mapper) {
-        this.metaGameService = metaGameService;
+    public PlayerController(ReloadableBeansManager beansManager, PlayerHttpMapper mapper) {
+        this.beansManager = beansManager;
         this.mapper = mapper;
     }
 
     @Override
     public ResponseEntity<PlayerProgressResponse> getPlayerProgress() {
-        var progress = this.metaGameService.getPlayerProgress();
+        var progress = this.beansManager.getMetaGameService().getPlayerProgress();
         PlayerProgressResponse response = new PlayerProgressResponse();
 
         response.setCompletedLevels(progress.getCompletedLevelIds().size());
@@ -43,7 +43,7 @@ public class PlayerController implements PlayerApi {
 
     @Override
     public ResponseEntity<List<AvailableLevel>> getAvailableLevels() {
-        List<com.towerdefense.progression.service.AvailableLevel> levels = this.metaGameService.getAvailableLevels();
+        List<com.towerdefense.progression.service.AvailableLevel> levels = this.beansManager.getMetaGameService().getAvailableLevels();
         List<AvailableLevel> dtos = levels.stream().map(l -> {
             AvailableLevel level = new AvailableLevel();
             level.setId(l.id());
@@ -58,7 +58,7 @@ public class PlayerController implements PlayerApi {
 
     @Override
     public ResponseEntity<GameConfiguration> prepareLevel(@PathVariable("levelId") String levelId) {
-        GameConfig config = this.metaGameService.prepareLevel(levelId);
+        GameConfig config = this.beansManager.getMetaGameService().prepareLevel(levelId);
         GameConfiguration gameConfig = new GameConfiguration();
         gameConfig.setLevelConfig(mapper.toHttp(config.level()));
         gameConfig.setTowersConfig(mapper.toTowersHttp(config.towers()));
@@ -70,9 +70,9 @@ public class PlayerController implements PlayerApi {
     public ResponseEntity<LevelCompletionResponse> completeLevel(@PathVariable("levelId") String levelId,
             @RequestBody LevelCompletionRequest request) {
 
-        int previousPoints = this.metaGameService.getPlayerProgress().getUpgradePoints();
-        this.metaGameService.onLevelComplete(levelId, request.getStars());
-        int newPoints = this.metaGameService.getPlayerProgress().getUpgradePoints();
+        int previousPoints = this.beansManager.getMetaGameService().getPlayerProgress().getUpgradePoints();
+        this.beansManager.getMetaGameService().onLevelComplete(levelId, request.getStars());
+        int newPoints = this.beansManager.getMetaGameService().getPlayerProgress().getUpgradePoints();
         int earned = newPoints - previousPoints;
 
         LevelCompletionResponse response = new LevelCompletionResponse();
